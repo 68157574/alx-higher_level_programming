22d3b54e0163436157443ee6ab0cd3b8e3be85eb
68157574 <abebemillion999@gmail.com>
2022-09-21 13:22:23 +0000

    0x01. Python - if/else, loops, functions

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,66 +1,46 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
-
-* print_listint - prints all elements of a listint_t list
-* @h: pointer to head of list
-* Return: number of nodes
+*insert_node - inserts number in sorte list
+*@head: addres of head
+*@number: number to insert
+*Return: on success addres of new node, NULL if failed
 */
-
-size_t print_listint(const listint_t *h)
+listint_t *insert_node(listint_t **head, int number)
 {
-const listint_t *current;
-unsigned int n; /* number of nodes */
-current = h;
-n = 0;
-while (current != NULL)
-{
-printf("%i\n", current->n);
-current = current->next;
-n++;
-}
-return (n);
-}
-/**
-* add_nodeint_end - adds a new node at the end of a listint_t list
-* @head: pointer to pointer of first node of listint_t list
-* @n: integer to be included in new node
-* Return: address of the new element or NULL if it fails
-*/
-listint_t *add_nodeint_end(listint_t **head, const int n)
-{
-listint_t *new;
-listint_t *current;
-current = *head;
-new = malloc(sizeof(listint_t));
-if (new == NULL)
+listint_t *current = *head;
+listint_t *temp;
+listint_t *new = malloc(sizeof(listint_t));
+if(!head)
 return (NULL);
-new->n = n;
-new->next = NULL;
-if (*head == NULL)
-*head = new;
-else
+if(!new)
+return (NULL);
+new->n = number;
+if (*head == NULL || number < (*head)->n)
 {
-while (current->next != NULL)
-current = current->next;
-current->next = new;
-}
+temp = *head;
+*head = new;
+new->next = temp;
 return (new);
 }
-/**
-* free_listint - frees a listint_t list
-* @head: pointer to list to be freed
-* Return: void
-*/
-void free_listint(listint_t *head)
+while (current)
 {
-listint_t *current;
-while (head != NULL)
+if (current->next == NULL)
 {
-current = head;
-head = head->next;
-free(current);
+current->next == new;
+return (new);
 }
+if (number < current->next->)
+{
+temp = current->next;
+current->next = new;
+new->next = temp;
+return (new);
+}
+current = current->next;
+}
+free(new);
+return (NULL);
 }
