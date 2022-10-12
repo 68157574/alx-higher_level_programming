dc7e046dddca26b9bc4a2d89508e73205554d2e8
68157574 <abebemillion999@gmail.com>
2022-10-12 12:33:44 +0000

    0x06. Python - Classes and Objects

--- 0x06-python-classes/0-main.py
+++ 0x06-python-classes/0-main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Define a class Square."""
-class Square:
-"""Represent a square."""
-pass
+Square = __import__('0-square').Square
+my_square = Square()
+print(type(my_square))
+print(my_square.__dict__)
