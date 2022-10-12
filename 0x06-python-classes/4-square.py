f58b19e937f63733c39a940e67f1cb8b5a1a6307
68157574 <abebemillion999@gmail.com>
2022-10-12 11:29:50 +0000

    0x06. Python - Classes and Objects

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,28 +1,17 @@
 #!/usr/bin/python3
-"""a class Square that defines a square
-by: (based on 3-square.py)
-"""
 class Square:
-"""Square class with a private attribute -
-size.
-"""
 def __init__(self, size=0):
-"""Initializes the size variable as a private
-instance artribute
-"""
-self.__size = size
+self.size = size
 @property
 def size(self):
-"""Instantiation with optional size of square"""
 return self.__size
 @size.setter
-def size(self, size_value):
-"""Gets the size of the square"""
-self.__size = size_value
-if not isinstance(size_value, int):
+def size(self, value):
+if type(value) != int:
 raise TypeError("size must be an integer")
-elif size_value < 0:
+elif value < 0:
 raise ValueError("size must be >= 0")
+self.__size = value
 def area(self):
-"""Returns the current square area"""
-return self.__size ** 2
+a = self.__size * self.__size
+return a
--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,35 +1,24 @@
 #!/usr/bin/python3
-"""Write a class Square that defines a square by:
-(based on 4-square.py)
-"""
 class Square:
-"""Square class with a private attribute -
-size.
-"""
 def __init__(self, size=0):
-"""Initializes the size variable as a private
-instance artribute
-"""
-self.__size = size
+self.size = size
 @property
 def size(self):
-"""Instantiation with optional size of square"""
 return self.__size
 @size.setter
-def size(self, size_value):
-"""Gets the size of the square"""
-self.__size = size_value
-if not isinstance(size_value, int):
+def size(self, value):
+if type(value) != int:
 raise TypeError("size must be an integer")
-elif size_value < 0:
+elif value < 0:
 raise ValueError("size must be >= 0")
+self.__size = value
 def area(self):
-"""Returns the current square area"""
-return self.__size ** 2
+a = self.__size * self.__size
+return a
 def my_print(self):
-"""prints in stdout the square with the character '#'"""
-for i in range(self.size):
-[print("#", end="") for i in range(self.size)]
+if self.__size == 0:
 print("")
-if self.size == 0:
-print("")
+for i in range(0, self.__size):
+for j in range(0, self.__size):
+print("#", end="")
+print()
